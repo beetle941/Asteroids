3a52ba9e8d8936570e727b4a6fb0c8d1a2da50b8
beetle941 <adrianmurillo892005@gmail.com>
2024-09-18 23:42:10 -0500

    10: finish spitting

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             for shot in shots:
                 if shot.check_collision(obj):
                     shot.kill()
-                    obj.kill()
+                    obj.split()
 
 
         screen.fill("black")
